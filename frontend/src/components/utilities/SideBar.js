56823d13767f750b534e74098c962f59acba283c
Anugrah <anugrahgupta.52@gmail.com>
2024-06-12 03:33:41 +0530

    made few ui changes in navigation and removed scroll from home

--- frontend/src/components/utilities/SideBar.js
+++ frontend/src/components/utilities/SideBar.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SideBar.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      <ul>
-        <li><Link to="/">Dashboard</Link></li>
-        <li><Link to="/ToDo">ToDo</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/domains">Domains</Link></li>
+  const [isOpen, setIsOpen] = useState(false);
 
-      </ul>
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : 'collapsed'}`}>
+      <button className={`toggle-button ${isOpen ? '' : 'collapsed'}`} onClick={toggleSidebar}>
+        <MenuIcon />
+      </button>
+        <br />
+        <br />
+      <div>
+      {isOpen && (
+        <ul>
+          <li><Link to="/">Dashboard</Link></li>
+          <li><Link to="/ToDo">ToDo</Link></li>
+          <li><Link to="/Scheduler">Calendar</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/domains">Domains</Link></li>
+        </ul>
+      )}
+      </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
