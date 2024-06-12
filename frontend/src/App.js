f23810d2f5ba4e26cee427a5e6e0b908027f5ec5
Anugrah <anugrahgupta.52@gmail.com>
2024-06-13 02:26:36 +0530

    reverted one mistake

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,9 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import TaskDetails from './components/screens/task_details_screen/taskdetail';
 import { Home } from './components/screens/home/home';
 import Sidebar from './components/utilities/SideBar';
+import TodoComponent from './components/screens/home/todo';
 import CalendarPage from './components/screens/month/CalandarPage';
 
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Contact() {
-  return <h2>Contact</h2>;
-}
 
 function App() {
   return (
@@ -25,10 +18,12 @@ function App() {
         <div className="main-content">
           <Routes>
             <Route path="/" index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
             <Route path="/task_details/:taskId" element={<TaskDetails />} />
             <Route path="/calendar" element={<CalendarPage />} /> {/* Add calendar route */}
+            <Route path="/ToDo" element={<TodoComponent/>} />
+            <Route path="/domains" element={<TodoComponent/>} />
+            <Route path="/projects" element={<TodoComponent/>} />          
+            <Route path="/task_details/:taskId" element={<TaskDetails />} />
           </Routes>
         </div>
       </div>
